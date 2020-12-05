d303a914fd98b790284532869e61198dd8ad7744
Dempsey FP <dempseyfinley@gmail.com>
2020-12-05 17:02:10 -0500

    directory structure created

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+// Importing the model (burger.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+--  In this file, write insert queries to
+--  populate the `burgers` 
+-- table with at least three entries.
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Triple-Bypass Burger"), ("Big Buford"), ("Impossible Whopper");
+
