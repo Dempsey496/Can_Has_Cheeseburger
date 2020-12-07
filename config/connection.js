5b2329116f3bb8ee5c4d3df1b456d8f8b92542f1
Dempsey FP <dempseyfinley@gmail.com>
2020-12-07 14:01:51 -0500

    connecting to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Babyblue1",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "i1xlanc2pmk00rc8",
+        password: "fxdap1s1q6ra6juf",
+        database: "c9k5xdoydhd14qov"
+    })
+};
 
 connection.connect(function(err) {
   if (err) {
